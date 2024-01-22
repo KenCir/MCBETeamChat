364d5a033f78b453b1a973d32aac2fd3bc620fe0
Ken_Cir <65713982+KenCir@users.noreply.github.com>
2024-01-22 20:15:12 +0900

    全体改良

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { world, system, EntityQueryOptions } from "@minecraft/server";
+import { world, system, EntityQueryOptions, Player } from "@minecraft/server";
 import config from "config";
 
+let systemFlag = false;
 const opQuery: EntityQueryOptions = {
   tags: [config.opTag],
 };
@@ -18,33 +19,54 @@ function buildTeamQuery(team: number, excludeOp: boolean): EntityQueryOptions {
   };
 }
 
+function setupTeam(): void {
+  for (const team of config.team.teams) {
+    const players = world.getPlayers(buildTeamQuery(team.score, false));
+    for (const player of players) {
+      player.nameTag = team.nameTagFormat.replaceAll(
+        "{player.name}",
+        player.name
+      );
+    }
+  }
+}
+
+function resetTeam(): void {
+  const players = world.getAllPlayers();
+  for (const player of players) {
+    player.nameTag = player.name;
+  }
+}
+
 system.afterEvents.scriptEventReceive.subscribe((event) => {
+  let resultMsg: string = null;
   switch (event.id) {
     case "team:set":
-      {
-        for (const team of config.team.teams) {
-          const players = world.getPlayers(buildTeamQuery(team.score, false));
-          for (const player of players) {
-            player.nameTag = team.nameTagFormat.replaceAll(
-              "{player.name}",
-              player.name
-            );
-          }
-        }
-      }
+      setupTeam();
+      resultMsg = "プレイヤーのチーム名をセットしました";
       break;
     case "team:reset":
-      {
-        const players = world.getAllPlayers();
-        for (const player of players) {
-          player.nameTag = player.name;
-        }
-      }
+      resetTeam();
+      resultMsg = "プレイヤーのチーム名をリセットしました";
       break;
+    case "team:start":
+      systemFlag = true;
+      resultMsg = "TeamChatを開始しました";
+      break;
+    case "team:stop":
+      systemFlag = false;
+      resultMsg = "TeamChatを停止しました";
+      break;
+  }
+
+  if (event.sourceEntity instanceof Player && resultMsg) {
+    event.sourceEntity.sendMessage(resultMsg);
   }
 });
 
 world.beforeEvents.chatSend.subscribe((event) => {
+  if (!systemFlag) return;
+
   const teamScoreboard = world.scoreboard.getObjective(config.team.scoreboard);
   if (!teamScoreboard) return;
 
@@ -73,4 +95,32 @@ world.beforeEvents.chatSend.subscribe((event) => {
   }
 
   event.cancel = true;
+});
+
+world.afterEvents.playerSpawn.subscribe((event) => {
+  if (!event.initialSpawn) return;
+  if (systemFlag) {
+    const teamScoreboard = world.scoreboard.getObjective(
+      config.team.scoreboard
+    );
+    if (!teamScoreboard) return;
+
+    let team = null;
+    try {
+      team = teamScoreboard.getScore(event.player.scoreboardIdentity);
+    } catch (e) {
+      return;
+    }
+    if (!team) return;
+
+    const teamConfig = config.team.teams.find((t) => t.score === team);
+    if (!teamConfig) return;
+
+    event.player.nameTag = teamConfig.nameTagFormat.replaceAll(
+      "{player.name}",
+      event.player.name
+    );
+  } else {
+    event.player.nameTag = event.player.name;
+  }
 });
